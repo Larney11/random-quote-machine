153c88455610fd3dae652c5fbbd1deb0607e8ab9
Lar Harney <lar@Lars-MacBook-Air.local>
2020-11-24 10:27:53 +0000

    text animation added. quote fades out and is replaced by new quote fading in;

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ let quotesData;
 var currentQuote = '',
   currentAuthor = '';
 
+
 function getQuotes() {
   
   const url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
@@ -16,7 +17,6 @@ function getQuotes() {
     success: function(data) {
       if(typeof data === 'string') {
         quotesData = JSON.parse(data);
-        getQuote();  
       }
     }
   });
@@ -24,12 +24,21 @@ function getQuotes() {
 
 function getQuote() {
 
-  let randomQuote = quotesData.quotes[Math.floor(Math.random() * quotesData.quotes.length)];
+  let randomQuote = quotesData.quotes[
+    Math.floor(Math.random() * quotesData.quotes.length)
+  ];
   currentQuote = randomQuote.quote;
   currentAuthor = randomQuote.author;
 
-  $('#text').text(currentQuote);
-  $('#author').text(currentAuthor);
+  $('#quote-text').animate({opacity: 0}, 300, function() {
+    $(this).animate({opacity:1}, 300);
+    $('#text').text(currentQuote);
+  });
+
+  $('#quote-author').animate({opacity: 0}, 300, function() {
+    $(this).animate({opacity: 1}, 300);
+    $('#author').html(currentAuthor);
+  })
 };
 
 $(document).ready(function() {
